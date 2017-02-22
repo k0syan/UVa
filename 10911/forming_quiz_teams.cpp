9303ea6140b389b51c4c83d9b44b322ef2865084
shahenkosyan <shahen.qosyan@gmail.com>
2017-02-23 00:54:09 +0400

    Forming Quiz Teams - Input

--- 10911/forming_quiz_teams.cpp
+++ 10911/forming_quiz_teams.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main () {
+  int i = 1;
+  while (true) {
+    int n;
+    cin >> n;
+
+    if (n == 0) {
+      break;
+    }
+    
+    vector<pair<int, int>> coords;
+    int count = n * 2;
+    while (count > 0) {
+      string name;
+      int x, y;
+      cin >> name >> x >> y;
+      pair<int, int> coord = {x, y};
+      coords.push_back(coord);
+      --count;
+    }
+
+    cout << "Case " << i << ":" << endl;
+    ++i;
+  }
+
   return 0;
 }
